d393c88b10400954c4827b4347dabf89cb732681
Danya-ti <danya.nschnts@gmail.com>
2021-03-16 01:27:48 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
-}
+    let a = n.toString()
+    console.log("a1", a);
+    let b = a.split("");
+    console.log("split", b);
+    let c = b.reverse();
+    console.log("reverse", c);
+    let d = c.join("");
+    console.log("join", d);
+    let e = parseInt(d);
+ return e;
+ }
